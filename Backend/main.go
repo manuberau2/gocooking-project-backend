a4e9cec6c24904073b863970ef39bd3ca115322e
Manuberau2 <manuberau@hotmail.com>
2025-01-01 22:24:08 -0300

    change dockerfile

--- Backend/main.go
+++ Backend/main.go
@@ -1,15 +1,16 @@
-package handler
+package main
 
 import (
+	"gocooking-backend/clients"
 	"gocooking-backend/handlers"
+	"gocooking-backend/middlewares"
 	"gocooking-backend/repositories"
 	"gocooking-backend/service"
-	"net/http"
+	"log"
 
 	"github.com/gin-gonic/gin"
 )
 
-// Global variables
 var (
 	router           *gin.Engine
 	alimentosHandler *handlers.AlimentoHandler
@@ -17,13 +18,15 @@ var (
 	compraHandler    *handlers.CompraHandler
 )
 
-func init() {
+func main() {
 	router = gin.Default()
 	dependencies()
 	mappingRoutes()
+	log.Println("Iniciando servidor en el puerto 8080...")
+	router.Run(":8080")
+
 }
 
-// dependencies initializes the dependencies like database and services
 func dependencies() {
 	var database repositories.DB
 
@@ -34,41 +37,39 @@ func dependencies() {
 	var alimentosService service.AlimentoInterface
 	var recetasService service.RecetaInterface
 	var comprasService service.CompraInterface
-
-	// Initialize repositories
+	//Inyectar repositorios
 	database = repositories.NewMongoDB()
 	alimentosRepository = repositories.NewAlimentoRepository(database)
 	recetasRepository = repositories.NewRecetaRepository(database)
 	comprasRepository = repositories.NewCompraRepository(database)
-
-	// Initialize services
+	//Inyectar servicios
 	alimentosService = service.NewAlimentoService(alimentosRepository)
 	recetasService = service.NewRecetaService(recetasRepository)
 	comprasService = service.NewCompraService(comprasRepository)
-
-	// Initialize handlers
+	//Inyectar handlers
 	alimentosHandler = handlers.NewAlimentoHandler(alimentosService)
 	recetasHandler = handlers.NewRecetaHandler(recetasService)
 	compraHandler = handlers.NewCompraHandler(comprasService)
+
 }
 
-// mappingRoutes defines all the routes for the application
 func mappingRoutes() {
-	/*authClients := clients.NewAuthClient()
+	authClients := clients.NewAuthClient()
 	authMiddleware := middlewares.NewAuthMiddleware(authClients)
 	router.Use(middlewares.CORSMiddleware())
-	router.Use(authMiddleware.ValidateToken)*/
-
-	// Alimentos routes
+	router.Use(authMiddleware.ValidateToken)
+	//Ruta alimentos
 	groupAlimentos := router.Group("/alimentos")
+
 	groupAlimentos.GET("/", alimentosHandler.GetAlimentos)
 	groupAlimentos.GET("/:id", alimentosHandler.GetAlimentoByID)
 	groupAlimentos.POST("/", alimentosHandler.InsertAlimento)
 	groupAlimentos.PUT("/:id", alimentosHandler.UpdateAlimento)
 	groupAlimentos.DELETE("/:id", alimentosHandler.DeleteAlimento)
 
-	// Recetas routes
+	//Ruta recetas
 	groupRecetas := router.Group("/recetas")
+
 	groupRecetas.GET("/", recetasHandler.GetRecetas)
 	groupRecetas.GET("/:id", recetasHandler.GetRecetaByID)
 	groupRecetas.GET("/buscar", recetasHandler.GetRecetasByParameters)
@@ -76,21 +77,17 @@ func mappingRoutes() {
 	groupRecetas.PUT("/:id", recetasHandler.UpdateReceta)
 	groupRecetas.DELETE("/:id", recetasHandler.DeleteReceta)
 
-	// Compras routes
+	//Ruta compras
 	groupCompras := router.Group("/compras")
+
 	groupCompras.GET("/", compraHandler.GetCompras)
 	groupCompras.GET("/productos-cantidad", compraHandler.GetProductosPorCantidadMinima)
 	groupCompras.POST("/", compraHandler.PostNuevaCompra)
 
-	// Reportes routes
 	groupReportes := router.Group("/reportes")
+
 	groupReportes.GET("/recetas-momento", recetasHandler.GetCantidadRecetasPorMomento)
 	groupReportes.GET("/recetas-tipo-alimento", recetasHandler.GetCantidadRecetasPorTipoAlimento)
 	groupReportes.GET("/costo-promedio-mes", compraHandler.GetCostoPromedioPorMesUltimoAnio)
-}
 
-// Vercel will call this function
-func Handler(w http.ResponseWriter, r *http.Request) {
-	// Use Gin to serve the request
-	router.ServeHTTP(w, r)
 }
